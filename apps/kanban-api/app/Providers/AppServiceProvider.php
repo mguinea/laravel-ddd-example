02c2e6e2bacac4eedecd23243f809222dc378ca2
Marc Guinea <develop.marcguinea@gmail.com>
2021-10-24 19:53:28 +0200

    fix: move service provide to laravel instead of src

--- apps/kanban-api/app/Providers/AppServiceProvider.php
+++ apps/kanban-api/app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
     }
 }
