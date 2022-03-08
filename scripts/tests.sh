2d1ee6edcfc27decfb8f916edebb74d23d87c718
Marc Guinea <Marc.Guinea@navigaglobal.com>
2022-03-08 11:40:25 +0100

    fix: minor phpunit adjustments

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -5,4 +5,4 @@ source "./etc/envs/commons.env"
 
 # Execute tests
 # docker exec -it laravel-ddd-example.kanban-api php vendor/bin/phpunit apps/kanban-api/tests --order-by=random
-docker exec -it laravel-ddd-example.app php vendor/bin/phpunit tests --order-by=random
+docker exec -it laravel-ddd-example.app php vendor/bin/phpunit tests --order-by=random --testsuite unit-tests
