3f37659256310078b84f8f86bbbcd48a0af19bc4
Marc Guinea <develop.marcguinea@gmail.com>
2022-10-14 13:58:47 +0200

    feat: add filter attributes

--- src/Shared/Domain/Criteria/Filter.php
+++ src/Shared/Domain/Criteria/Filter.php
@@ -6,7 +6,25 @@ namespace App\Shared\Domain\Criteria;
 
 final class Filter
 {
-    public function __construct()
+    public function __construct(
+        private string $field,
+        private FilterOperator $operator,
+        private mixed $value
+    ) {
+    }
+
+    public function field(): string
     {
+        return $this->field;
+    }
+
+    public function operator(): FilterOperator
+    {
+        return $this->operator;
+    }
+
+    public function value(): mixed
+    {
+        return $this->value;
     }
 }
